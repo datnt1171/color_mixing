0137476140f8829ad649c538abaf46a083a2d3a8
datnt1171 <157688888+datnt1171@users.noreply.github.com>
2024-09-23 16:22:36 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 23 11:05:24 2024
+
+@author: KT1
+"""
+
+import dash
+from dash import Dash, html, dcc
+
+app = Dash(__name__, use_pages=True)
+
+app.layout = html.Div([
+    html.H1('Multi-page app with Dash Pages'),
+    html.Div([
+        html.Div(
+            dcc.Link(f"{page['name']} - {page['path']}", href=page["relative_path"])
+        ) for page in dash.page_registry.values()
+    ]),
+    dash.page_container
+])
+
+if __name__ == '__main__':
+    app.run(debug=True)
